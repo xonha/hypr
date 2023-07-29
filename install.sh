747e09b55fcd63b641617007c25b8780b7f67119
henrique <holiveira@wattio.com.br>
2023-07-29 18:13:14 -0300

    refactor: scripts clean up

--- install.sh
+++ install.sh
@@ -1,31 +1,46 @@
 #!/usr/bin/env bash
 
-sudo pacman -S \
-hyprland \
-grim \
-slurp \
-swappy \
-swaylock \
-swaybg \
-wofi \
-mako \
-neovim \
-waybar \
-lxappearance \
-nemo \
-nemo-python \
-nemo-preview \
-nemo-fileroller \
-nemo-audio-tab \
-light \
-xfce-polkit \
-discord \
-qalculate-gtk \
-guake \
-kitty \
-fish \
-wget \
-catppuccin-gtk-theme-mocha \
-papirus-folders-catppuccin-git \
-&& papirus-folders -C cat-mocha-peach --theme Papirus-Dark \
-&& bash -c "$(curl https://raw.githubusercontent.com/fuwwy/Discord-Screenshare-Linux/main/scripts/install.sh -sSfL)" \
+# List of packages to install
+packages=(
+    catppuccin-gtk-theme-mocha
+    discord
+    fish
+    github-cli
+    google-chrome
+    grim
+    guake
+    hyprland
+    keeweb-desktop-bin
+    kitty
+    light
+    lxappearance
+    mako
+    mpv
+    nemo
+    nemo-audio-tab
+    nemo-fileroller
+    nemo-preview
+    nemo-python
+    neovim
+    papirus-folders-catppuccin-git
+    qalculate-gtk
+    slurp
+    stremio
+    swaybg
+    swaylock
+    swappy
+    visual-studio-code-bin
+    wofi
+    wget
+    waybar
+    xfce-polkit
+)
+
+# Install packages using yay with --noconfirm option
+yay -S --noconfirm "${packages[@]}"
+
+# Apply the Papirus-Dark theme to Papirus-Folders
+papirus-folders -C cat-mocha-peach --theme Papirus-Dark
+
+# Install Discord Screenshare Linux script
+bash -c "$(curl https://raw.githubusercontent.com/fuwwy/Discord-Screenshare-Linux/main/scripts/install.sh -sSfL)"
--- uninstall.sh
+++ uninstall.sh
@@ -1,33 +1,43 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sudo pacman -R \
-archcraft-neofetch \
-archcraft-ranger \
-archcraft-help \
-archcraft-about \
-archcraft-vim \
-archcraft-openbox \
-xfce4-power-manager \
-xfce4-notifyd \
-xfce4-settings \
-xfce4-terminal \
-obmenu-generator \
-obconf \
-openbox \
-plank \
-rofi \
-xcolor \
-alacritty \
-ranger \
-tint2 \
-mplayer \
-mpd \
-vim \
-simplescreenrecorder \
-picom \
-meld \
-arandr \
-xarchiver \
-nitrogen \
-galculator \
-atril \
+# List of packages to uninstall
+packages=(
+    archcraft-neofetch
+    archcraft-omz
+    archcraft-hooks-zsh
+    archcraft-ranger
+    archcraft-help
+    archcraft-about
+    archcraft-vim
+    archcraft-openbox
+    xfce4-power-manager
+    xfce4-notifyd
+    xfce4-settings
+    xfce4-terminal
+    obmenu-generator
+    obconf
+    openbox
+    plank
+    rofi
+    xcolor
+    alacritty
+    ranger
+    tint2
+    mplayer
+    mpd
+    vim
+    simplescreenrecorder
+    picom
+    meld
+    arandr
+    xarchiver
+    nitrogen
+    galculator
+    atril
+    zsh
+)
+
+# Loop through the packages and uninstall them one by one
+for package in "${packages[@]}"; do
+    yay -Rns "$package" --noconfirm
+done

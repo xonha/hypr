9d54841e9623e11f80098e4bfad9841513c4dfac
Henrique <holiveira@wattio.com>
2023-08-11 13:21:37 -0300

    removed mellowplayer

--- .scripts/gtkthemes.sh
+++ .scripts/gtkthemes.sh
@@ -4,7 +4,7 @@
 
 THEME='Catppuccin-Mocha-Standard-Mauve-Dark'
 ICONS='Papirus-Dark'
-FONT='JetBrainsMono Nerd Font 10'
+FONT='JetBrains Mono 10'
 CURSOR='Qogirr-Dark'
 
 SCHEMA='gsettings set org.gnome.desktop.interface'
